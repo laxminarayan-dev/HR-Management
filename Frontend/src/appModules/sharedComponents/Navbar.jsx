b1a0aa041771a0b7c96e90fd91f5ea02bae060dc
Laxmi Narayan <jaiswallucky0112@gmail.com>
2025-07-19 23:19:19 +0530

    website name changed

--- Frontend/src/appModules/sharedComponents/Navbar.jsx
+++ Frontend/src/appModules/sharedComponents/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar({ isSidebarOpen, setIsSidebarOpen }) {
     <nav className="flex justify-between items-center bg-white px-6 py-4 shadow-sm">
       <div>
         <Link to="/">
-          <h1 className="text-lg font-bold font-serif">Admin Dashboard</h1>
+          <h1 className="text-lg font-bold font-serif">HRFusion</h1>
         </Link>
       </div>
       <div>
